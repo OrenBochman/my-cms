cde4a1064c79fac6dc6ef2885e77368b2cf2623c
oren bochman <OrenBochman@gmail.com>
2018-01-14 08:47:19 +0200

    remove inlining

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ const del = require('del');
 const gulp = require('gulp');
 const polymerBuildUtils = require('gulp-polymer-build-utils');
 const runSequence = require('run-sequence');
-gulp.task('default', cb => {
-  return runSequence('dist:prepare', 'inline-references', cb);
-});
+const yargs = require('yargs');
+const argv = polymerBuildUtils.addYargs(yargs).help().argv;
+gulp.task('dist:clean', () => del('dist'));
 gulp.task('dist:prepare', ['dist:clean'], () => {
   const config = require('./polymer.json');
   return polymerBuildUtils.build(config)()
     .pipe(gulp.dest('dist'));
 });
-gulp.task('dist:clean', () => del('dist'));
+gulp.task('default', cb => {
+  //return runSequence('dist:prepare', 'inline-references', cb);
+  return runSequence('dist:prepare',  cb);
+});
+gulp.task('dist:test', function() {
+  return polymerBuildUtils.runWct(argv);
+});
